eb64a7031903c0e4ae2b76c09b18cd39e4585586
alexandre <alexcg.devv@gmail.com>
2025-01-10 17:52:51 -0500

    janvier 2025 (nouveau logo)

--- ogref-app/src/App.js
+++ ogref-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import About from './components/About';
@@ -16,12 +16,20 @@ function App() {
     return (
         <Router>
             <div className="App">
+                {/* Navbar toujours affichée */}
                 <Navbar onContactClick={scrollToFooter} />
+
+                {/* Routes */}
                 <Routes>
                     <Route path="/" element={<Carousel />} />
                     <Route path="/About" element={<About />} />
                     <Route path="/Contact" element={<Contact />} />
+
+                    {/* Redirection automatique vers '/' */}
+                    <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
+
+                {/* Footer toujours affiché */}
                 <Footer ref={footerRef} />
             </div>
         </Router>
